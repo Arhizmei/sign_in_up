847f79c7e07f9333cc90e98f341b6f21f4e872f5
Arhizmei <arhizmei@gmail.com>
2023-03-14 21:57:58 +0200

    1.2

--- app/src/main/java/com/zmei/sign_up_in/MainActivity.kt
+++ app/src/main/java/com/zmei/sign_up_in/MainActivity.kt
@@ -2,11 +2,13 @@ package com.zmei.sign_up_in
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zmei.sign_up_in.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bind : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        late
+        bind = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(bind.root)
     }
 }
--- app/src/main/java/com/zmei/sign_up_in/MainActivity2.kt
+++ app/src/main/java/com/zmei/sign_up_in/MainActivity2.kt
@@ -2,10 +2,15 @@ package com.zmei.sign_up_in
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zmei.sign_up_in.databinding.ActivityMain2Binding
+import com.zmei.sign_up_in.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var bind : ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
+        bind = ActivityMain2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(bind.root)
     }
 }
