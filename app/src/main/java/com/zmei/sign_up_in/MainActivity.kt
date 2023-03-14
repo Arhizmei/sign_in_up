4b7a3d6ca461bc751a57c9906346bcb24933c123
Arhizmei <arhizmei@gmail.com>
2023-03-14 21:45:43 +0200

    init

---
+++ app/src/main/java/com/zmei/sign_up_in/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zmei.sign_up_in
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        late
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
